f4701e2150a3affb60ba0b14b8b4e8075a47a6a1
rassu <senyopan@hotmail.com>
2023-10-02 00:22:21 +0900

    課題クリア

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,33 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const promise = new Promise((resolve) => {
+    setTimeout(resolve, 3000);
+  });
+  await promise;
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
+function test(data) {
+  let fullName = data.map(function (element) {
+    element["full_name"] = element.family_name + " " + element.first_name;
+  });
+  console.log(data);
+  return data;
 }
-
